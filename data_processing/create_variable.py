87322e422ad1889deca99bc1e4a85a89c64d8da9
Janet Pan <ypan23@uw.edu>
2023-11-17 13:58:08 -0800

    created new varaibles and merge with patient meta dataframe, still need to merge with physician characteristics

--- data_processing/create_variable.py
+++ data_processing/create_variable.py
@@ -2,15 +2,31 @@ import pandas as pd
 import numpy as np
 import os
 
-# load in data 
-data = pd.read_excel("../data/deid_cea_v2.xlsx")
+### OUTPU: 'META' DATAFRAME FOR THE FOLLOWING STPES
 
-# Create a new dataframe for condensed patient information 
-new_df = pd.DataFrame()
+## load in data 
+patient_visit = pd.read_excel("../data/deid_cea_v2.xlsx") # Patient revisit after surveillance 
+patient_meta = pd.read_csv("../data/Final dataset prep_072521.csv") # All features data
 
-# Calculate the probability of the patient consistenly returning every three months (90 days)
-data['return'] = np.where(data.groupby('PID')['dx2cea'].diff() <= 90, 1, 0)
-new_df['return_probability'] = data.groupby('PID')['return'].mean()
-new_df = new_df.reset_index()
+## Merging patient visit and patient meta
+meta = patient_meta.merge(patient_visit,on="PID", how='outer').sort_index()
 
-new_df.to_csv('../data/deid_cea_v2_condensed.csv', index=False)
+
+## Creating all variables 
+# How long from the start of surveillance
+meta['days_from_surveil'] = meta['dx2cea'] - meta['dx2surveildate'] 
+
+# How long from the previous visit
+meta['days_from_last_visit'] = meta['dx2cea'].diff() 
+
+# How long from the start of surveillance to their first visit
+meta['first_visit_from_surveil'] = meta.groupby('PID').head(1)['dx2cea'] - meta.groupby('PID').head(1)['dx2surveildate']
+
+# CEA value from previous visit 
+meta['cea_prev_visit'] = meta['value'].shift(1)
+
+# If CEA value >5, then higher chance of reoccurrence
+meta['chances_of_recur'] = np.where(meta['dx2cea'] > 5, 1, 0)
+
+# Patient revisits every 90 days 
+meta['return_visit'] = np.where(meta.groupby('PID')['dx2cea'].diff() <= 90, 1, 0)
