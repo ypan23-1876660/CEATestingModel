65694b85a0edea5aaede4bbcc8b7f8338c27b956
mjoshi2@uw.edu <mahima@Mahima>
2023-12-07 12:23:23 -0800

    most updated smoke tests for test_data_clean

--- data_processing/test_data_clean.py
+++ data_processing/test_data_clean.py
@@ -2,33 +2,51 @@ import numpy as np
 import pandas as pd
 
 from create_variable import create_var, combine_physid
-from data_clean import data_impute, remove_nan, encode_df
+from data_clean import data_impute, remove_nan, clean_rename_patPhyInfo, scale_patPhyInfo, encode_df
 from sklearn.preprocessing import OneHotEncoder
 
 import unittest 
+
+patient_visit = pd.read_csv("../data/deid_cea_v2.csv")
+patient_info = pd.read_csv("../data/Final dataset prep_072521.csv")
+patient = create_var(patient_visit, patient_info)
+directory = ("../data")
+pattern = ("*md*.csv")
+phys = combine_physid(directory, pattern)
+df = patient.merge(phys, how="left", left_on="physid_x", right_on="physid").drop(columns=['physid_x', 'physid_y'])
+
 
 class TestDataImpute(unittest.TestCase):
     """Running unittest on data_impute"""
     def test_data_impute(self):
         """Smoke test: ensuring the data_impute function is working"""
-        patient_visit = pd.read_csv("../data/deid_cea_v2.csv")
-        patient_info = pd.read_csv("../data/Final dataset prep_072521.csv")
-        df = create_var(patient_visit, patient_info)
-        result = data_impute(df)
+        result = data_impute(patient)
         self.assertIsNotNone(result)
     
         
 class TestRemoveNan(unittest.TestCase):
     """Running unittest on remove_nan"""
     def test_remove_nan(self):
-        """Smoke test: ensuring the data inpute function is working"""
-        patient_visit = pd.read_csv("../data/deid_cea_v2.csv")
-        patient_info = pd.read_csv("../data/Final dataset prep_072521.csv")
-        patient = create_var(patient_visit, patient_info)
-        directory = ("../data")
-        pattern = ("*md*.csv")
-        phys = combine_physid(directory, pattern)
-        df = patient.merge(phys, how="left", left_on="physid_x", right_on="physid").drop(columns=['physid_x', 'physid_y'])
+        """Smoke test: ensuring the remove_nan function is working"""
         result = remove_nan(df)
         self.assertIsNotNone(result)
+        
+class TestCleanRenamepatPthyInfo(unittest.TestCase):
+    """Smoke test: ensuring the clean_rename_patPhyInfo function is working"""
+    def test_clean_rename_patPthyInfo(self):
+        result = clean_rename_patPhyInfo(df)
+        self.assertIsNotNone(result)
+        
+class TestScalepatPthyInfo(unittest.TestCase):
+    """Smoke test: ensuring the scale_pathPhyInfo function is working"""
+    def test_scale_pathPhyInfo(self):
+        result = scale_patPhyInfo(df)
+        self.assertIsNotNone(result)
+
+class TestEncode_df(unittest.TestCase):
+    """Smoke test: ensuring the encode_df function is working"""
+    def test_encode_df(self):
+        result = encode_df(df)
+        self.assertIsNotNone(result)
+        
         
