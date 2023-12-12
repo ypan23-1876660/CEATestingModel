a6557bc1e5351698f21f6dc24d80af77e75d6bb6
Janet Pan <ypan23@uw.edu>
2023-12-12 13:31:30 -0800

    split the scripts for two use cases

--- scripts/model_train.py
+++ scripts/model_train.py
@@ -1,23 +1,21 @@
-# import pandas as pd
-# import numpy as np
-
-# import glob
-# from functools import reduce
-# import sys
-# import streamlit as st
-
-#sys.path.append("ml4cea")
+"""
+Use the input data to train the model 
+"""
 import os
 import pickle
 from ml4cea import create_var, combine_physid, data_impute, export_df, model_create, show_predict_page
 
 # -------- USER INPUT ------- # 
 PATH = "data/"
-PATIENT_VISTI = "deid_cea_v2.csv"
-PATIENT_INFOR = "Final dataset prep_072521.csv"
-MODEL_NAME = "model.pkl"
-MODEL_PATH = os.path.join(PATH, MODEL_NAME)
+PATIENT_VISTI = "deid_cea_v2.csv" # default training data 
+PATIENT_INFOR = "Final dataset prep_072521.csv" # default training data
+OUTPUT_PATH = "data/output/" # Update this path if wanting to use other input data; default = default_output
+MODEL_NAME = "model.pkl" # update this name if want to use different name 
+MODEL_PATH = os.path.join(OUTPUT_PATH, MODEL_NAME)
 # --------------------------- #
+# create a new folder if using user data 
+if not os.path.exists(OUTPUT_PATH):
+    os.makedirs(OUTPUT_PATH)
 
 # Combine patient visit inforamtion and patient meta information 
 PATEINT_VISIT_PATH = os.path.join(PATH,PATIENT_VISTI)
@@ -35,7 +33,7 @@ phys_meta = combine_physid(PATH)
 patient_phys_info = patient_df_reduced.merge(phys_meta, how="left", left_on="physid_x", right_on="physid").drop(columns=['physid_x', 'physid_y']) # use "patient_phys_info" dataframe to continue working in dataframe
 
 # Export proessed dataframe for modeling 
-cleaned_patient_phys_info = export_df(patient_phys_info)
+cleaned_patient_phys_info = export_df(patient_phys_info, OUTPUT_PATH)
 
 # Fit model and save the model in data folder
 model_create(cleaned_patient_phys_info, MODEL_PATH)
