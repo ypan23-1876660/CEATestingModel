7d38de05903a7e5f067fbb257d1dcb76b8c290e6
Janet Pan <ypan23@uw.edu>
2023-12-12 22:00:11 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import find_packages, setup
 
+with open("requirements.txt") as file:
+    requirements = file.read().splitlines()
+
 setup(
     name="ml4cea",
     packages=find_packages(),
+    install_requires=requirements
 )
